f87e1f67c3709ec78de599dbb4a7ffcf47c3774d
Nya <nyabentley@gmail.com>
2023-04-16 13:04:12 -0400

    Modeled module

---
+++ Assets/Scripts/Girlfriend.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+
+public class Girlfriend : MonoBehaviour {
+
+   public KMBombInfo Bomb;
+   public KMAudio Audio;
+
+    public AudioClip English;
+    public AudioClip French;
+    public AudioClip German;
+    public AudioClip Italian;
+    public AudioClip Japanese;
+    public AudioClip Mandarin;
+    public AudioClip Portuguese;
+    public AudioClip Spanish;
+
+   static int ModuleIdCounter = 1;
+   int ModuleId;
+   private bool ModuleSolved;
+
+   void Awake () {
+      ModuleId = ModuleIdCounter++;
+      /*
+      foreach (KMSelectable object in keypad) {
+          object.OnInteract += delegate () { keypadPress(object); return false; };
+      }
+      */
+
+      //button.OnInteract += delegate () { buttonPress(); return false; };
+
+   }
+
+   void Start () {
+
+   }
+
+   void Update () {
+
+   }
+
+#pragma warning disable 414
+   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+   IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+   }
+
+   IEnumerator TwitchHandleForcedSolve () {
+      yield return null;
+   }
+}
